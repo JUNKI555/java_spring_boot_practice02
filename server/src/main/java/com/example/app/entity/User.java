194b104ff7b5291e43cf05cbc8f18d5233279427
JUNKI555 <junki555@gmail.com>
2020-12-20 23:49:48 +0900

    :sparkles: fix add users error

--- server/src/main/java/com/example/app/entity/User.java
+++ server/src/main/java/com/example/app/entity/User.java
@@ -11,7 +11,7 @@ import javax.persistence.Table;
 public class User {
 
   @Id
-  @GeneratedValue(strategy = GenerationType.AUTO)
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
   private String name;
 
